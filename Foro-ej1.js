0ba7132b89a01230403822f767a585d3b5cc0e79
Bryan Alejandro Aguilar Calderon <115672960+Bryan24352@users.noreply.github.com>
2022-10-21 17:50:31 -0600

    Add files via upload

---
+++ Foro-ej1.js
@@ -0,0 +1,16 @@
+
+
+   var edad=18;
+
+    console.log("Que edad tienes:  "+edad)
+
+
+    console.log(".........................")
+
+    if(edad>=18){
+       console.log("Si, puedes entrar a la fiesta");
+
+    }
+    else {
+    console.log("No puedes pasar, hasta la proxima... ");
+    }
